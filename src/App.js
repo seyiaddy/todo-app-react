c2ce048b72d6f10c2d7ee582fc7060599cf4c2fb
Seyi <oluwaseyiadisaa@gmail.com>
2020-12-26 21:14:55 +0100

    basic todo functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState(["Take dogs for a walk", "Take the rubbish out"]);
+  const [todos, setTodos] = useState([
+    "Take dogs for a walk",
+    "Take the rubbish out",
+  ]);
   const [input, setInput] = useState("");
 
+  const addTodo = (e) => {
+    e.preventDefault(); // to prevent the page from reloading
+    setTodos([...todos, input]);
+    setInput("");
+  };
 
   return (
     <div className="App">
-      <h1>Todo App</h1>
-      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
-      <button>Add Todo</button>
+      <h1>Todo</h1>
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
 
       <ul>
-        {todos.map(todo => (
+        {todos.map((todo) => (
           <li>{todo}</li>
         ))}
       </ul>
-
     </div>
   );
 }
